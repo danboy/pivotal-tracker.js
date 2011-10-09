5c28aee9ea9329907c476bf17f97bb139eafa358
Dan Nawara <dnawara@groupon.com>
2011-10-08 21:05:48 -0500

    Super basic stuff is working

---
+++ main.js
@@ -0,0 +1,70 @@
+var request = require('request')
+    , parse = require('xml2json');
+
+console.log('in pivotal tracker');
+
+Pivotal = {
+  getToken: function(username,password,callback){
+    self = this;
+    var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+    var token = null;
+    request({
+        url: 'https://www.pivotaltracker.com/services/v3/tokens/active'
+      , headers: {
+          'Authorization': auth
+        }
+    },function(err,resp,body){
+      if (!err && resp.statusCode == 200) {
+        body = parse.toJson(body,{object: true});
+        token = body.token.guid;
+        callback(token);
+      }
+    });
+  }
+  , getProjects: function(token,callback){
+    this.makeRequest(
+        'https://www.pivotaltracker.com/services/v3/projects'
+      , token
+      , function(results){
+        callback(results);
+      }
+    );
+
+  }
+  , getProject: function(tracker,token,callback){
+    this.makeRequest(
+        'https://www.pivotaltracker.com/services/v3/projects/'+tracker
+      , token
+      , function(results){
+        callback(results);
+      }
+    );
+
+  }
+  , getCurrentIteration: function(tracker,token,callback){
+    this.makeRequest(
+        'https://www.pivotaltracker.com/services/v3/projects/'+tracker+'/iterations/current_backlog'
+      , token
+      , function(results){
+        callback(results);
+      }
+    );
+
+  }
+  , makeRequest: function( url, token, callback ){
+    request({
+        url: url
+      , headers: {
+          'X-TrackerToken': token
+        }
+    },function(err,resp,body){
+      if (!err && resp.statusCode == 200) {
+        body = parse.toJson(body,{object: true});
+        callback(body);
+      }
+    });
+  
+  }
+}
+
+module.exports = Pivotal;
